cac8a5481417586d19c0042c2905da45b31e6c81
Cláudio Schirmer Guedes <claudioschirmer@icloud.com>
2024-04-07 16:43:50 -0400

    layout fix

--- src/components/AddComponentModal/style.js
+++ src/components/AddComponentModal/style.js
@@ -67,6 +67,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
+    marginTop: 15,
   },
   switchText: {
     fontSize: 18,
